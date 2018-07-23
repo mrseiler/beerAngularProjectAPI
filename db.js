e54d2ce5c7ffdff702353e518799cdb4840f4c47
masonseiler <masonseiler1@gmail.com>
2018-07-23 11:30:36 -0400

    a

--- db.js
+++ db.js
@@ -1,31 +1,16 @@
-<<<<<<< HEAD
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize(process.env.DBNAME, process.env.PGUSER, process.env.PGPASS, {
-    dialect: "postgres",
-    port: 5432,
-    host: process.env.PGHOST
-=======
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.PGNAME, process.env.PGUSER, process.env.PGPASS, {
     host: process.env.PGHOST,
     dialect: 'postgres',
     port:5432
->>>>>>> 44d9eeac98dbb2336ca6557eda8e857a03871818
 });
 
 sequelize.authenticate().then(
     function() {
-<<<<<<< HEAD
-        console.log("connected to the database");
-    },
-    function(err) {
-=======
         console.log('Connected to holdmybeer postgres database');
     },
     function(err){
->>>>>>> 44d9eeac98dbb2336ca6557eda8e857a03871818
         console.log(err);
     }
 );
